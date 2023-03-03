28a21c3527f12671832d764ff4969ff6f336c522
Adeniyi Olanrewaju <adeniyiboladale@yahoo.com>
2023-03-03 23:07:13 +0000

    formatted

--- market/__init__.py
+++ market/__init__.py
@@ -1,31 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager #Always pip install flask_logi, flask_bcrypt,
+from flask_login import LoginManager  # Always pip install flask_logi, flask_bcrypt,
 import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir,'market.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'market.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 app.config['SECRET_KEY'] = '05f280bfdd256ec4b00a6ace'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = "login_page" 
-login_manager.login_message_category="info"
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
-
-
-
-
-
-
-    
-    
-
--- run.py
+++ run.py
@@ -1,15 +1,12 @@
 from market import app
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
-if __name__=='__main__':
-    app. run(debug=True)
-    
-    
-    
-    #use the following steps to add items to your item list
+    # use the following steps to add items to your item list
     # from market import app
     # from market.models import db, Item
     # i1 = Item(name='Iphone 11', price='900', barcode='776767677676', description='This is an Iphone 11 promax')  
     # with app.app_context():
-    #db.session.add(i1) 
-    #db.session.commit()
+    # db.session.add(i1)
+    # db.session.commit()
